bb33d384887f3a2783cf6260e53a2b3642a99317
Corinne Alini <coryalini@gmail.com>
2022-04-16 18:08:28 -0400

    pusing stuff

--- src/DataReader.py
+++ src/DataReader.py
@@ -7,7 +7,6 @@ import torch
 class DataReader:
     input_path ="../sem_seg_h5/"
     def __init__(self, input_path="../sem_seg_h5/"):
-        super(self)
         self.input_path=input_path
     def read_data(self,data_name="/Chair-1/test-00.h5"):
         filename = self.input_path + data_name
@@ -27,6 +26,12 @@ class DataReader:
             print("length ",torch.max( torch.tensor(f['label_seg'][2, :])))
             utils.viz_seg (torch.tensor(f['data'][2, :, :]), torch.tensor(f['label_seg'][2, :]), "test.gif", "cuda")
             return torch.tensor(f['data']), torch.tensor(f['label_seg'])
+    def read_to_file(self):
+        to = torch.load("gan_generator_voxels_chairs.to")
 
+        # print(to.shape)
+        print(to)
 dataReader = DataReader()
-dataReader.read_data("/Chair-1/test-00.h5")
+# dataReader.read_to_file()
+data, labels = dataReader.read_data("/Chair-1/test-00.h5")
+np.savez("chair_ex",data[2, :, :])
---
+++ src/SegmentSwapper.py
@@ -0,0 +1,18 @@
+import h5py
+import numpy as np
+import utils
+import torch
+
+# class SegmentSwapper:
+#
+#     def __init__(self):
+#         return
+#
+#     def __change_segment(self,obj1_pt_cloud,obj2_pt_cloud_segment,obj1_labels):
+#
+#
+#
+#     def swap_segments_random(self,obj1_pt_cloud,obj1_labels,obj2_pt_cloud,obj2_labels):
+#
+
+
