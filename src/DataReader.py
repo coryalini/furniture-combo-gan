dad393d8ec4108c47ada2be44b61ed56726e7b73
Corinne Alini <coryalini@gmail.com>
2022-04-16 13:08:36 -0400

    refactored

---
+++ src/DataReader.py
@@ -0,0 +1,32 @@
+import h5py
+import numpy as np
+import utils
+import torch
+
+# filename = "../sem_seg_h5/Chair-1/test-00.h5"
+class DataReader:
+    input_path ="../sem_seg_h5/"
+    def __init__(self, input_path="../sem_seg_h5/"):
+        super(self)
+        self.input_path=input_path
+    def read_data(self,data_name="/Chair-1/test-00.h5"):
+        filename = self.input_path + data_name
+        with h5py.File(filename, "r") as f:
+            # List all groups
+            print("Keys: %s" % f.keys())
+            a_group_key = list(f.keys())[0]
+
+            # Get the data
+            data = list(f[a_group_key])
+
+            print(f['data'])
+            print(np.max(f['label_seg']))
+            print(f['data_num'])
+            # for i in data:
+            #     print(i, type(i))
+            print("length ",torch.max( torch.tensor(f['label_seg'][2, :])))
+            utils.viz_seg (torch.tensor(f['data'][2, :, :]), torch.tensor(f['label_seg'][2, :]), "test.gif", "cuda")
+            return torch.tensor(f['data']), torch.tensor(f['label_seg'])
+
+dataReader = DataReader()
+dataReader.read_data("/Chair-1/test-00.h5")
