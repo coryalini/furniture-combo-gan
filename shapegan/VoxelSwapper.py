55a66811bfd1d876628f88b11c0362ab4b42a37c
Ubuntu <ubuntu@ip-172-31-37-162.us-east-2.compute.internal>
2022-04-19 18:28:02 +0000

    validity test case

--- shapegan/VoxelSwapper.py
+++ shapegan/VoxelSwapper.py
@@ -1,14 +1,37 @@
 
+import pytorch3d
+import torch
 
 class VoxelSwapper:
 
     def __init__(self):
         pass
 
+
     def swap_voxel_vertical(self, chair_voxel, table_voxel):
+
+        validity = {"chair":"valid", "table":"valid"}
+        mesh, faces = pytorch3d.marching_cubes(chair_voxel)
+
+        if mesh.shape[0] == 0:
+            validity["chair"] = "invlid"
+
+        mesh, faces = pytorch3d.marching_cubes(table_voxel)
+        if mesh.shape[0] == 0:
+            validity["table"] = "invlid"
+
         tmp_voxel = chair_voxel
         tmp_voxel[0:4,:,:] = table_voxel[0:4,:,:]
-        return tmp_voxel
 
+        return tmp_voxel, validity
 
-    # def swapVoxels_horizontal(self):
+    
+vox = VoxelSwapper()
+chair_voxel = torch.randint(512, (8, 8, 8))
+table_voxel = torch.randint(512, (8, 8, 8))
+
+new_vox, validity = vox.swap_voxel_vertical(chair_voxel, table_voxel)
+
+print(new_vox)
+print(validity)
+
