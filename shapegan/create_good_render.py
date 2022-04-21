eb385476a169ecab296a723af2956440cb640121
Corinne Alini <coryalini@gmail.com>
2022-04-20 20:45:10 -0400

    oh corinne

--- shapegan/create_good_render.py
+++ shapegan/create_good_render.py
@@ -9,16 +9,18 @@ from model import LATENT_CODE_SIZE, LATENT_CODES_FILENAME
 import random
 from util import device
 
+
 class ImageGrid():
-    def __init__(self, width, height=1, cell_width = 3, cell_height = None, margin=0.2, create_viewer=True, crop=True):
+    def __init__(self, width, height=1, cell_width=3, cell_height=None, margin=0.2, create_viewer=True, crop=True):
         print("Plotting...")
         self.width = width
         self.height = height
         cell_height = cell_height if cell_height is not None else cell_width
 
         self.figure, self.axes = plt.subplots(height, width,
-            figsize=(width * cell_width, height * cell_height),
-            gridspec_kw={'left': 0, 'right': 1, 'top': 1, 'bottom': 0, 'wspace': margin, 'hspace': margin})
+                                              figsize=(width * cell_width, height * cell_height),
+                                              gridspec_kw={'left': 0, 'right': 1, 'top': 1, 'bottom': 0,
+                                                           'wspace': margin, 'hspace': margin})
         self.figure.patch.set_visible(False)
 
         self.crop = crop
@@ -28,13 +30,13 @@ class ImageGrid():
         else:
             self.viewer = None
 
-    def set_image(self, image, x = 0, y = 0):
+    def set_image(self, image, x=0, y=0):
         cell = self.axes[y, x] if self.height > 1 and self.width > 1 else self.axes[x + y]
         cell.imshow(image)
         cell.axis('off')
         cell.patch.set_visible(False)
 
-    def set_voxels(self, voxels, x = 0, y = 0, color=None):
+    def set_voxels(self, voxels, x=0, y=0, color=None):
         if color is not None:
             self.viewer.model_color = color
         self.viewer.set_voxels(voxels)
@@ -49,7 +51,7 @@ class ImageGrid():
             self.viewer.delete_buffers()
 
 
-def load_sdf_net(filename=None, return_latent_codes = False):
+def load_sdf_net(filename=None, return_latent_codes=False):
     from model.sdf_net import SDFNet, LATENT_CODES_FILENAME
     sdf_net = SDFNet()
     if filename is not None:
@@ -75,10 +77,11 @@ COUNT = 5
 codes = standard_normal_distribution.sample([COUNT, LATENT_CODE_SIZE]).to(device)
 
 plot = ImageGrid(COUNT, create_viewer=False)
-
+angles = np.linspace(0, 360, 15)
 for im in range(COUNT):
     images = []
-    for i in range(15):
-        image = render_image(generator, codes[i, :], radius=1.6, crop=True, sdf_offset=-0.045, vertical_cutoff=1)
+    for a in angles:
+        image = render_image(generator, codes[im, :], radius=1.6, crop=True, sdf_offset=-0.045,
+                             vertical_cutoff=1, angle=a)
         images.append(image)
     imageio.mimsave(f"plots/results_{im}.gif", [np.uint8(im * 255) for im in all_images])
